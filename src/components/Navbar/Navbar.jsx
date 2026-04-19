57d28d633c45d03eb0088d5126796f49cb4b9019
Florian Siqani <102537748+floriangota@users.noreply.github.com>
2026-04-19 16:15:26 +0200

    Fix scrolling jitter with native CSS smooth scroll API

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState, useContext } from 'react'
 import './Navbar.css'
 import logooo from '../../assets/logooo.png'
 import { ThemeContext } from '../../ThemeContext'
-import { Link } from 'react-scroll';
 
 const Navbar = () => {
 
@@ -26,12 +25,12 @@ const Navbar = () => {
     <nav className={`app-container ${sticky ? 'dark-nav' : ''}`}>
       <img src={logooo} alt="" className='logo' />
       <ul className={mobileMenu ? 'show-mobile-menu' : ''}> 
-        <li><Link to='front' smooth={true} offset={0} duration={500} onClick={() => setMobileMenu(false)}>Home</Link></li>
-        <li><Link to='programs' smooth={true} offset={-260} duration={500} onClick={() => setMobileMenu(false)}>Programs</Link></li>
-        <li><Link to='about' smooth={true} offset={-150} duration={500} onClick={() => setMobileMenu(false)}>About us</Link></li>
-        <li><Link to='campus' smooth={true} offset={-260} duration={500} onClick={() => setMobileMenu(false)}>Campus</Link></li>
-        <li><Link to='testimonials' smooth={true} offset={-260} duration={500} onClick={() => setMobileMenu(false)}>Testimonials</Link></li>
-        <li><Link to='contact' smooth={true} offset={-260} duration={500} onClick={() => setMobileMenu(false)}>Contact us</Link></li>
+        <li><a href='#front' onClick={() => setMobileMenu(false)}>Home</a></li>
+        <li><a href='#programs' onClick={() => setMobileMenu(false)}>Programs</a></li>
+        <li><a href='#about' onClick={() => setMobileMenu(false)}>About us</a></li>
+        <li><a href='#campus' onClick={() => setMobileMenu(false)}>Campus</a></li>
+        <li><a href='#testimonials' onClick={() => setMobileMenu(false)}>Testimonials</a></li>
+        <li><a href='#contact' onClick={() => setMobileMenu(false)}>Contact us</a></li>
       </ul>
       <div className="nav-right">
         <button className="theme-toggle" onClick={toggleTheme} title={darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}>
